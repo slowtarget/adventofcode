67454e5198caffa6e7e32a8115749d7ae78eade9
slowtarget <cogitateth@gmail.com>
2022-01-04 23:34:50 +0000

    format

--- aoc2021/src/day15/index.ts
+++ aoc2021/src/day15/index.ts
@@ -11,61 +11,61 @@ var FgRed = "\x1b[31m"
 const toKey = (x: number, y: number): string => {
   return `(${x},${y})`;
 }
-const numToRightJustifiedString = (num:number, length:number):string => {
+const numToRightJustifiedString = (num: number, length: number): string => {
   var s = num.toString(10);
 
-  return s.padStart(length,' ');
+  return s.padStart(length, ' ');
 }
 
 class Point {
-  public iteration:number = 0;
+  public iteration: number = 0;
   public leastCost: number = Number.MAX_VALUE;
   public lowestCostNeighbour?: Point;
   public neighbours: Point[] = [];
   constructor(
-      public value: number,
-      public x: number,
-      public y: number
+    public value: number,
+    public x: number,
+    public y: number
   ) { }
 
   public clone(): Point {
-      return new Point(this.value, this.x, this.y);
+    return new Point(this.value, this.x, this.y);
   }
 
   public toString(): string {
-      return toKey(this.x, this.y);
+    return toKey(this.x, this.y);
   }
 }
 
 class Route {
   public cost: number;
   constructor(
-      public route: Point[]
+    public route: Point[]
   ) {
-      this.cost = route.reduce((p: number, c, i) => p + (i === 0 ? 0 : c.value), 0);
+    this.cost = route.reduce((p: number, c, i) => p + (i === 0 ? 0 : c.value), 0);
   }
 
   public clone(): Route {
-      return new Route(this.route.map(p => p.clone()));
+    return new Route(this.route.map(p => p.clone()));
   }
 }
 type Points = { [key: string]: { point: Point } };
 const adjacent = [-1, 0, 1];
 const beside = adjacent.map(x => adjacent.map(y => ({ dx: x, dy: y })))
-      .flatMap(a => a)
-      .filter(a => a.dx | a.dy) // remove centre
-      .filter(a=> Math.abs(a.dx) ^ Math.abs(a.dy)); // remove diagonals
+  .flatMap(a => a)
+  .filter(a => a.dx | a.dy) // remove centre
+  .filter(a => Math.abs(a.dx) ^ Math.abs(a.dy)); // remove diagonals
 
-const shortest = (point:Point):Point[] => {
+const shortest = (point: Point): Point[] => {
   if (point.lowestCostNeighbour) {
-      return [point, ...shortest(point.lowestCostNeighbour!)]
+    return [point, ...shortest(point.lowestCostNeighbour!)]
   } else {
-      return [point];
+    return [point];
   }
 }
 // Returns current time
 // (and, if provided, prints the event's name)
-const now = (eventName:string|null = null) => {
+const now = (eventName: string | null = null) => {
   if (eventName && debug) {
     console.log(`Started ${eventName}..`);
   }
@@ -78,10 +78,10 @@ let started = now();
 
 // Returns time elapsed since `beginning`
 // (and, optionally, prints the duration in seconds)
-const elapsed = (beginning:number = start, log:boolean = false) => {
+const elapsed = (beginning: number = start, log: boolean = false) => {
   const duration = new Date().getTime() - beginning;
   if (log && debug) {
-      console.log(`${duration/1000}s`);
+    console.log(`${duration / 1000}s`);
   }
   return duration;
 }
@@ -93,99 +93,99 @@ class Puzzle {
   public grid: Point[][];
 
   constructor(
-      public points: Point[]
+    public points: Point[]
   ) {
-      elapsed(start,true);
-      start = now("mapping");
-      // this.pointMap = this.points.reduce((p, c) => ({ ...p, [c.toString()]: { point: c } }), {}); 35 seconds!
-      
-      this.points.forEach(c=>{this.pointMap[c.toString()]= { point: c } });
-      elapsed(start,true);
-      start = now("grid");
-      this.start = this.pointMap[toKey(0, 0)].point;
-      var maxX = 0, maxY = 0;
-      for (var point of this.points) {
-          if(point.x>maxX) {
-              maxX = point.x;
-          }
-          if(point.y>maxY) {
-              maxY = point.y;
-          }
-      }
+    elapsed(start, true);
+    start = now("mapping");
+    // this.pointMap = this.points.reduce((p, c) => ({ ...p, [c.toString()]: { point: c } }), {}); 35 seconds!
 
-      const destinationKey = toKey(maxX, maxY);
-      this.destination = this.pointMap[destinationKey].point;
-      this.grid = [];
-      for (var y = 0; y < this.destination.y + 1; y ++) {
-          this.grid[y] = [];
-          for (var x = 0; x < this.destination.x + 1; x ++) {
-              this.grid[y][x] = this.pointMap[toKey(x, y)].point;
-          }
+    this.points.forEach(c => { this.pointMap[c.toString()] = { point: c } });
+    elapsed(start, true);
+    start = now("grid");
+    this.start = this.pointMap[toKey(0, 0)].point;
+    var maxX = 0, maxY = 0;
+    for (var point of this.points) {
+      if (point.x > maxX) {
+        maxX = point.x;
       }
-      elapsed(start,true);
-      start = now("neighbours");
+      if (point.y > maxY) {
+        maxY = point.y;
+      }
+    }
 
-      this.points.forEach(point => {
-          point.neighbours = beside.map(({ dx, dy }) => this.pointMap[toKey(point.x + dx, point.y + dy)]?.point).filter(a => !!a);
-      });
-      elapsed(start,true);
+    const destinationKey = toKey(maxX, maxY);
+    this.destination = this.pointMap[destinationKey].point;
+    this.grid = [];
+    for (var y = 0; y < this.destination.y + 1; y++) {
+      this.grid[y] = [];
+      for (var x = 0; x < this.destination.x + 1; x++) {
+        this.grid[y][x] = this.pointMap[toKey(x, y)].point;
+      }
+    }
+    elapsed(start, true);
+    start = now("neighbours");
+
+    this.points.forEach(point => {
+      point.neighbours = beside.map(({ dx, dy }) => this.pointMap[toKey(point.x + dx, point.y + dy)]?.point).filter(a => !!a);
+    });
+    elapsed(start, true);
   }
 
   public solve(): number {
-      // Inputs: cost field raster, source location, destination location (most implementations can solve for multiple sources and destinations simultaneously)
+    // Inputs: cost field raster, source location, destination location (most implementations can solve for multiple sources and destinations simultaneously)
 
-      // Accumulation: Starting at the source location compute the lowest total cost needed to reach every other cell in the grid. 
-      // Although there are several algorithms, such as those published by Eastman and Douglas,[8][9] they generally follow a similar strategy.[13]
-      //  This process also creates, as an important byproduct, a second raster grid usually called the backlink grid (Esri) or movement direction grid (GRASS), 
-      //   in which each cell has a direction code (0-7) representing which of its eight neighbors had the lowest cost.
-      // Find a cell that is adjacent to at least one cell that already has an accumulated cost assigned (initially, this is only the source cell)
-      // Determine which neighbor has the lowest accumulated cost. Encode the direction from the target to the lowest-cost neighbor in the backlink grid.
-      // Add the cost of the target cell (or an average of the costs of the target and neighbor cells) to the neighbor accumulated cost, 
-      // to create the accumulated cost of the target cell. If the neighbor is diagonal, the local cost is multiplied by √2
-      // The algorithm must also take into account that indirect routes may have lower cost, often using a hash table to keep track of temporary cost values along the 
-      // expanding fringe of computation that can be reconsidered.
-      // Repeat the procedure until all cells are assigned.
+    // Accumulation: Starting at the source location compute the lowest total cost needed to reach every other cell in the grid. 
+    // Although there are several algorithms, such as those published by Eastman and Douglas,[8][9] they generally follow a similar strategy.[13]
+    //  This process also creates, as an important byproduct, a second raster grid usually called the backlink grid (Esri) or movement direction grid (GRASS), 
+    //   in which each cell has a direction code (0-7) representing which of its eight neighbors had the lowest cost.
+    // Find a cell that is adjacent to at least one cell that already has an accumulated cost assigned (initially, this is only the source cell)
+    // Determine which neighbor has the lowest accumulated cost. Encode the direction from the target to the lowest-cost neighbor in the backlink grid.
+    // Add the cost of the target cell (or an average of the costs of the target and neighbor cells) to the neighbor accumulated cost, 
+    // to create the accumulated cost of the target cell. If the neighbor is diagonal, the local cost is multiplied by √2
+    // The algorithm must also take into account that indirect routes may have lower cost, often using a hash table to keep track of temporary cost values along the 
+    // expanding fringe of computation that can be reconsidered.
+    // Repeat the procedure until all cells are assigned.
 
-      // work from the start to the end assigning initial costs
-      this.start.leastCost = 0;
-      this.start.iteration = 1;
-      // this.assignCostsToNeighbours(this.start)
+    // work from the start to the end assigning initial costs
+    this.start.leastCost = 0;
+    this.start.iteration = 1;
+    // this.assignCostsToNeighbours(this.start)
 
-      // work from the destination to the start and attempt to reduce journey cost
+    // work from the destination to the start and attempt to reduce journey cost
 
-      var iteration = 2;
-      var changed = 1;
-      while (changed && iteration<150) {
-          start = now(`iteration ${iteration}`);
-          changed = this.points
-              .map(a=>this.reducedCostJourney(a, iteration))
-              .reduce((p,c)=>p+c,0);
+    var iteration = 2;
+    var changed = 1;
+    while (changed && iteration < 150) {
+      start = now(`iteration ${iteration}`);
+      changed = this.points
+        .map(a => this.reducedCostJourney(a, iteration))
+        .reduce((p, c) => p + c, 0);
 
-          // console.log(`iteration ${iteration} changed: ${changed} shortest is ${this.destination.leastCost}`);
-          iteration++;
-          elapsed(start,debug);
-          
-      }
-      // console.log(this.toDots());
-      // Find a cell that is adjacent to at least one cell that already has an accumulated cost assigned (initially, this is only the source cell)
-      // Determine which neighbor has the lowest accumulated cost. Encode the direction from the target to the lowest-cost neighbor in the backlink grid.
-      // Add the cost of the target cell (or an average of the costs of the target and neighbor cells) to the neighbor accumulated cost, to create the accumulated cost of the target cell. If the neighbor is diagonal, the local cost is multiplied by √2
-      // The algorithm must also take into account that indirect routes may have lower cost, often using a hash table to keep track of temporary cost values along the expanding fringe of computation that can be reconsidered.
-      // Repeat the procedure until all cells are assigned.
+      // console.log(`iteration ${iteration} changed: ${changed} shortest is ${this.destination.leastCost}`);
+      iteration++;
+      elapsed(start, debug);
 
-      elapsed(started,debug);
-      return this.destination.leastCost;
+    }
+    // console.log(this.toDots());
+    // Find a cell that is adjacent to at least one cell that already has an accumulated cost assigned (initially, this is only the source cell)
+    // Determine which neighbor has the lowest accumulated cost. Encode the direction from the target to the lowest-cost neighbor in the backlink grid.
+    // Add the cost of the target cell (or an average of the costs of the target and neighbor cells) to the neighbor accumulated cost, to create the accumulated cost of the target cell. If the neighbor is diagonal, the local cost is multiplied by √2
+    // The algorithm must also take into account that indirect routes may have lower cost, often using a hash table to keep track of temporary cost values along the expanding fringe of computation that can be reconsidered.
+    // Repeat the procedure until all cells are assigned.
+
+    elapsed(started, debug);
+    return this.destination.leastCost;
   }
-  reducedCostJourney(point: Point, iteration:number):number {
-      var leastCost = Math.min(...point.neighbours.map(a=>a.leastCost||0));
-      point.iteration = iteration;
-      var changed = 0;
-      if (leastCost + point.value < point.leastCost) {
-          point.lowestCostNeighbour = point.neighbours.filter(a=>a.leastCost===leastCost)[0];
-          point.leastCost = leastCost + point.value;
-          changed ++;
-      }
-      return changed;
+  reducedCostJourney(point: Point, iteration: number): number {
+    var leastCost = Math.min(...point.neighbours.map(a => a.leastCost || 0));
+    point.iteration = iteration;
+    var changed = 0;
+    if (leastCost + point.value < point.leastCost) {
+      point.lowestCostNeighbour = point.neighbours.filter(a => a.leastCost === leastCost)[0];
+      point.leastCost = leastCost + point.value;
+      changed++;
+    }
+    return changed;
   }
   // assignCostsToNeighbours(point: Point) {
   //     point.neighbours.filter(a=>a.iteration===0).forEach(a=>{
@@ -194,55 +194,55 @@ class Puzzle {
   //         this.assignCostsToNeighbours(a);
   //     })
   // }
-  toString():string {
-      var result = "\n";
+  toString(): string {
+    var result = "\n";
 
-      var short = shortest(this.destination);
-      
-      for (var y = 0; y < this.destination.y + 1; y ++) {
-          for (var x = 0; x < this.destination.x + 1; x ++) {
-              var point = this.pointMap[toKey(x, y)].point;
-              var cell = numToRightJustifiedString(point.leastCost ?? 0, 4) + ` (${numToRightJustifiedString(point.value,1)})`;
-              if (short.includes(point)){
-                  cell = `${Bright}${FgRed}${cell}${Reset}`;
-              }
-              result += cell;
-          }
-          result += "\n";
+    var short = shortest(this.destination);
+
+    for (var y = 0; y < this.destination.y + 1; y++) {
+      for (var x = 0; x < this.destination.x + 1; x++) {
+        var point = this.pointMap[toKey(x, y)].point;
+        var cell = numToRightJustifiedString(point.leastCost ?? 0, 4) + ` (${numToRightJustifiedString(point.value, 1)})`;
+        if (short.includes(point)) {
+          cell = `${Bright}${FgRed}${cell}${Reset}`;
+        }
+        result += cell;
       }
-      return result;
+      result += "\n";
+    }
+    return result;
   }
-  toDots():string {
-      var result = "\n";
+  toDots(): string {
+    var result = "\n";
 
-      var short = shortest(this.destination);
-      
-      for (var y = 0; y < this.destination.y + 1; y ++) {
-          for (var x = 0; x < this.destination.x + 1; x ++) {
-              var point = this.pointMap[toKey(x, y)].point;
-              var cell = " ";
-              if (short.includes(point)){
-                  cell = `${Bright}${FgRed}${point.value}${Reset}`;
-              }
-              result += cell;
-          }
-          result += "\n";
+    var short = shortest(this.destination);
+
+    for (var y = 0; y < this.destination.y + 1; y++) {
+      for (var x = 0; x < this.destination.x + 1; x++) {
+        var point = this.pointMap[toKey(x, y)].point;
+        var cell = " ";
+        if (short.includes(point)) {
+          cell = `${Bright}${FgRed}${point.value}${Reset}`;
+        }
+        result += cell;
       }
-      return result;
+      result += "\n";
+    }
+    return result;
   }
 }
-const bumped:{[idx:number]:number} = {
-  0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:1, 11:2, 12:3, 13:4, 14:5, 15:6, 16:7, 17:8, 18:9
+const bumped: { [idx: number]: number } = {
+  0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 1, 11: 2, 12: 3, 13: 4, 14: 5, 15: 6, 16: 7, 17: 8, 18: 9
 };
 
 const parseInput = (rawInput: string) => {
   var points = rawInput
-        .replace(/\r\n/g, '\n')
-        .split(/\n/g)
-        .map((a, y) => a.split("").map(a => parseInt(a)).map((a, x) => new Point(a, x, y)))
-        .flatMap(a => a);
+    .replace(/\r\n/g, '\n')
+    .split(/\n/g)
+    .map((a, y) => a.split("").map(a => parseInt(a)).map((a, x) => new Point(a, x, y)))
+    .flatMap(a => a);
 
-    var puzzle = new Puzzle(points);
+  var puzzle = new Puzzle(points);
 
   return puzzle;
 }
@@ -259,15 +259,15 @@ const part2 = (rawInput: string) => {
 
   var width = input.destination.x + 1;
   var height = input.destination.y + 1;
-  var points2 = 
-  [[0,1,2,3,4],
-  [1,2,3,4,5],
-  [2,3,4,5,6],
-  [3,4,5,6,7],
-  [4,5,6,7,8]].map((row, mY)=>row
-                  .map((bump,mX)=>input.points
-                      .map(point=>new Point(bumped[point.value + bump], point.x + mX * width, point.y + mY * height))
-                  ).flatMap(a=>a)).flatMap(a=>a);
+  var points2 =
+    [[0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 5],
+    [2, 3, 4, 5, 6],
+    [3, 4, 5, 6, 7],
+    [4, 5, 6, 7, 8]].map((row, mY) => row
+      .map((bump, mX) => input.points
+        .map(point => new Point(bumped[point.value + bump], point.x + mX * width, point.y + mY * height))
+      ).flatMap(a => a)).flatMap(a => a);
 
   var puzzle2 = new Puzzle(points2);
   return puzzle2.solve();
