587ce57d98377ae3fa48ac0a70218e850fddf20c
slowtarget <cogitateth@gmail.com>
2022-12-01 07:39:53 +0000

    day 1

--- aoc2022/src/day01/index.ts
+++ aoc2022/src/day01/index.ts
@@ -1,29 +1,49 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput: string) => {
-  return rawInput.replace(/\r\n/g, "\n").split(/\n\n/g);
+  return rawInput.replace(/\r\n/g, "\n").split(/\n\n/g).map(s=>s.split(/\n/));
 };
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return 0;
+  const result = input.map(s => s.reduce((p, c) => p + Number(c), 0))
+    .reduce((p2, c2) => (p2 > c2 ? p2 : c2), 0);
+  return result;
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return 0;
+  const result = input.map(s => s.reduce((p, c) => p + Number(c), 0))
+      .sort((a, b) => b - a)
+      .slice(0,3).reduce((p,c)=>p+c,0);
+
+  return result;
 };
 
 const testInput = `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
 `;
 run({
   part1: {
     tests: [
       {
         input: testInput,
-        expected: 0,
+        expected: 24000,
       },
     ],
     solution: part1,
@@ -32,7 +52,7 @@ run({
     tests: [
       {
         input: testInput,
-        expected: 0,
+        expected: 45000,
       },
     ],
     solution: part2,
