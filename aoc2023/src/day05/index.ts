767e317f5eee0fae689922734fbdb3478e4784e2
Jason Farmer <jasonf@motabilityoperations.co.uk>
2023-12-05 23:00:23 +0000

    day 5 - part 1 passed, part2 test passing - running against full set is slow... too slow.. considering un-ramda-ing.

--- aoc2023/src/day05/index.ts
+++ aoc2023/src/day05/index.ts
@@ -1,7 +1,25 @@
 import run from "aocrunner";
 
-import {add, append, both, cond, converge, drop,
-    flatten, gt, head, identity, last, lte, map, pipe, range, split, splitEvery, T, tap} from "ramda";
+import {
+    add,
+    append,
+    both,
+    cond,
+    drop,
+    flatten,
+    gt,
+    head,
+    identity,
+    last,
+    lte,
+    map,
+    pipe,
+    range,
+    split,
+    splitEvery,
+    T,
+    tap
+} from "ramda";
 import * as fs from "fs";
 import assert from "assert";
 
@@ -9,12 +27,7 @@ import assert from "assert";
 const regex = /\d+/g;
 const getFunctionPair = (input: number[]):[(x:number) => boolean, (x:number) => number] =>  {
   const [destinationStart,sourceStart, length] = input;
-  const predicate = both(lte(sourceStart), gt(sourceStart + length));
-  const transform = (x:number) => {
-
-    return add(destinationStart - sourceStart)(x);
-  }
-  return [predicate, transform];
+  return [both(lte(sourceStart), gt(sourceStart + length)), add(destinationStart - sourceStart)];
 }
 
 const [predicate, transform] = getFunctionPair([50, 98, 2]);
@@ -49,14 +62,13 @@ tests.forEach(([source, expected]) => {
     assert(actual === expected);
 });
 type FunctionPair = [(x:number) => boolean, (x:number) => number];
-const mapLineToFunctionPair: (line: string) => FunctionPair = (line) => {
-    return pipe(
+const mapLineToFunctionPair: (line: string) => FunctionPair =
+    pipe(
         (line: string) => line.matchAll(regex),
         Array.from,
         map(parseInt),
         getFunctionPair
-    )(line);
-}
+    );
 
 const getDefaultFunctionPair: () => FunctionPair = () => [T, identity];
 
@@ -66,16 +78,15 @@ const mapLinesToFunctionPairs: (lines:string[]) => FunctionPair[] =
         append(getDefaultFunctionPair()),
     );
 
-type Transform = {name: string, transform: (x:number) => number;}
-const getTransform = (input: string[]): Transform => {
-  const name = head(input) ?? "not found"; //e.g. seed-to-soil map:
-  const rest: string[] = drop(1, input);
-  const transform: (x:number) => number = cond(mapLinesToFunctionPairs(rest));
-  return {name, transform};
-}
+const getTransform = pipe(
+      tap((x:string[])=>console.log({title:"getting transform", t:x[0]})),
+      drop(1),
+      mapLinesToFunctionPairs,
+      cond<any[], number>
+  );
 
-function getLocationsOfSeeds(pipeTransforms: ((x: number) => number)[], seeds: number[]): number[] {
-    return map(
+const getLocationsOfSeeds : (pipeTransforms: ((x: number) => number)[]) => (seeds:number[]) => number[] = (pipeTransforms: ((x: number) => number)[]) =>
+    map(
         pipe(
             pipeTransforms[0],
             pipeTransforms[1],
@@ -85,11 +96,10 @@ function getLocationsOfSeeds(pipeTransforms: ((x: number) => number)[], seeds: n
             pipeTransforms[5],
             pipeTransforms[6]
         )
-    )(seeds);
-}
+    );
 
-function getPipeTransforms(input: string[]) {
-    const transforms: Transform[] = pipe(
+
+const getPipeTransforms = pipe(
         (x: string[]) => drop(1, x),
         map(
             pipe(
@@ -97,12 +107,7 @@ function getPipeTransforms(input: string[]) {
                 getTransform
             )
         )
-    )(input);
-
-    const pipeTransforms: ((x: number) => number)[] = // map(prop('transform'))(transforms); cannot get this working...
-        transforms.map(x => x.transform);
-    return pipeTransforms;
-}
+    );
 
 const getLocations = (input: string[]):number[] => {
     const seeds = pipe(
@@ -113,17 +118,14 @@ const getLocations = (input: string[]):number[] => {
       Array.from,
       map(
         pipe(
-          (match: RegExpMatchArray) => {
-            const {0: value, index, input} = match;
-            return value
-          },
+          head,
           parseInt
         )
       )
     )(input);
     console.log({seeds});
     const pipeTransforms = getPipeTransforms(input);
-    return getLocationsOfSeeds(pipeTransforms, seeds);
+    return getLocationsOfSeeds(pipeTransforms)(seeds);
 }
 
 const getSeeds: (input: string[]) => number[] =
@@ -135,10 +137,7 @@ const getSeeds: (input: string[]) => number[] =
         Array.from,
         map(
             pipe(
-                (match: RegExpMatchArray) => {
-                    const {0: value, index, input} = match;
-                    return value
-                },
+                (match: RegExpMatchArray) => match[0],
                 parseInt
             )
         )
@@ -150,20 +149,31 @@ console.log("test seed ranges",pipe(
     flatten
 )([1,5,8,2])); // [1,2,3,4,5,8,9]
 const getLocationsFromRanges = (input: string[]):number[] => {
-
-    // maybe pass each range through the transform and find the min..
-    const s1 = getSeeds(input);
-    const seeds = pipe(
-        splitEvery(2),
-        map((y: number[])=>range(y[0], y[0] + y[1])),
-        flatten
-    )(s1);
-    console.log("part2 seeds length",seeds.length);
     const pipeTransforms = getPipeTransforms(input);
-    return getLocationsOfSeeds(pipeTransforms, seeds);
+    const transform = getLocationsOfSeeds(pipeTransforms);
+    return pipe(
+        getSeeds,
+        splitEvery(2),
+        map(tap((x:number[])=>console.log(x[1]))),
+        map(
+            (range: number[]) => {
+                console.log({title:"starting range ", range, now: Date.now()});
+                const [start, length] = range;
+                const end = start + length;
+                let min = Number.MAX_SAFE_INTEGER;
+                for (let i = start; i < end; i++) {
+                    const [transformed] = transform([i]);
+                    if (transformed < min) {
+                        min = transformed;
+                    }
+                }
+                return min;
+            }
+        )
+    )(input);
 }
 
-// theres probably some clever way to work this problem backwards ... find the lowest location range then see what maps to that ... etc etc
+// there's probably some clever way to work this problem backwards ... find the lowest location range then see what maps to that ... etc. etc.
 const part1 =
     pipe(
       split("\n\n"),
