b11678708ac8da9b3dff71caae9804e65dc80706
Jason Farmer <jasonf@motabilityoperations.co.uk>
2023-12-11 00:46:38 +0000

    day 9 - part 1 wrong answer - too low, but example passes

--- aoc2023/src/day09/index.ts
+++ aoc2023/src/day09/index.ts
@@ -1,26 +1,63 @@
 import run from "aocrunner";
+import {aperture, converge, identity, last, map, pipe, reduce, reverse, split, sum} from "ramda";
 
-const parseInput = (rawInput: string) => rawInput;
+const numbersRegex = /\d+/g;
+const numbersFromString = pipe(
+    (line: string) => line.matchAll(numbersRegex),
+    Array.from,
+    map(
+        pipe(
+            (match: RegExpMatchArray) => match[0],
+            parseInt
+        )
+    )
+)
+const splitIntoPairs:(input: number[])=>(number[][]) = aperture(2);
+const difference:(input:number[])=>number = (input: number[]) => input[1] - input[0];
 
-const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const getDifferences = pipe(
+    last,
+    splitIntoPairs,
+    map(difference)
+)
+const step:(sequences:number[][])=>number[][] = converge((diff:number[],sequences:number[][])=>{
+    if (diff.every(x=>x===0)) {
+        return [...sequences];
+    }
+    return step([...sequences, diff]);
+},[getDifferences,identity])
+const getLastNumber: (input:number[])=> number = last;
+const getNextInSequence = pipe(
+    (x)=>[x],
+    step,
+    map(getLastNumber),
+    sum
+);
 
-  return;
-};
+const part1 = pipe(
+    split("\n"),
+    map(
+        pipe(
+            numbersFromString,
+            getNextInSequence
+        )
+    ),
+    sum
+);
+// 340126608 too low
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
-};
-
+const part2 = (rawInput: string) => undefined;
+const input11=
+`0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: input11,
+        expected: 114,
+      },
     ],
     solution: part1,
   },
